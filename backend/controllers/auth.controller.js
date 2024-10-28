6e9e934c43850bb618cfd20adc0fdc88f200b204
iamyourdre <iamyourdre@gmail.com>
2024-10-28 09:09:54 +0700

    feat: login & logout

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -42,9 +42,37 @@ export const signup = async(req, res) => {
     res.status(500).json({error:"Something went wrong."});
   }
 }
-export const login = (req, res) => {
-  console.log("login")
+export const login = async(req, res) => {
+  try {
+
+    const {username, password} = req.body;
+    const user = await User.findOne({username});
+    const isPasswordValid = await bcrypt.compare(password, user?.password || "");
+
+    if(user && isPasswordValid){
+      generateToken(user._id, res);
+      res.status(200).json({
+        _id: user._id,
+        fullName: user.fullName,
+        username: user.username,
+        profilePic: user.profilePic
+      })
+    } else {
+      res.status(400).json({error:"Invalid username or password."})
+    }
+    
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).json({error:"Something went wrong."});
+  }
 }
 export const logout = (req, res) => {
-  console.log("logout")
+  try {
+    res.cookie("jwt", "", {maxAge: 0,});
+    res.status(200).json({message:"Logged out successfully"});
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).json({error:"Something went wrong."});
+    
+  }
 }

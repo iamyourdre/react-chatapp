f19a169ed07eaefd3c50c1f397e715206192cf9c
iamyourdre <iamyourdre@gmail.com>
2024-11-18 18:05:27 +0700

    add example environment file and update README with project details

--- frontend/src/components/sidebar/SearchInput.jsx
+++ frontend/src/components/sidebar/SearchInput.jsx
@@ -29,7 +29,7 @@ const SearchInput = () => {
       <label className="input input-bordered flex items-center gap-2 focus-within:text-slate-700 hover:bg-slate-100 focus-within:bg-slate-100 outline-none mx-3 my-3">
         <input type="text" className="grow" value={search} placeholder="Search" onChange={(e) => setSearch(e.target.value)} />
         {!search ? <LuSearch /> : <HiXMark onClick={(e) => setSearch('')}/>}
-          <button type="submit">Ok</button>
+          <button type="submit" className='hidden'></button>
       </label>
     </form>
   )
